35fa41601508feab223e0491b5688f1189637391
SunilShankaramurthy <77558713+SunilShankaramurthy@users.noreply.github.com>
2022-06-01 00:24:37 +0100

    Update MyRetry.java

--- src/main/java/com/ikea/automation/listeners/retry_mechanism/MyRetry.java
+++ src/main/java/com/ikea/automation/listeners/retry_mechanism/MyRetry.java
@@ -12,7 +12,9 @@ public class MyRetry implements IRetryAnalyzer {
     private static int retryCount = 0;
 
     public boolean retry(ITestResult result) {
-        int maxRetryCount= Integer.parseInt(System.getProperty("maxRetryCount"));
+        int maxRetryCount=0;
+        if(System.getProperty("maxRetryCount")!=null)
+            maxRetryCount= Integer.parseInt(System.getProperty("maxRetryCount"));
         if (retryCount < maxRetryCount) {
             retryCount++;
             return true;
